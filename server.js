04e4e808f2cfedd24915e683e0b96fc388fcc1d7
ysera34 <ycyoon34@gmail.com>
2017-09-11 14:08:14 +0900

    mysql config, pool, connection, query

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 var express = require('express');
-  // formidable = require('formidable');
+// formidable = require('formidable');
 var appdata = require('./data/appdata.js');
+var pool = require('./configs/connector.js');
 
 var app = express();
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars').create({
-    defaultLayout:'main',
-    helpers: {
-        section: function(name, options){
-            if(!this._sections) this._sections = {};
-            this._sections[name] = options.fn(this);
-            return null;
-        }
+  defaultLayout: 'main',
+  helpers: {
+    section: function(name, options) {
+      if (!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
     }
+  }
 });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -24,41 +25,57 @@ app.use(express.static(__dirname + '/public'));
 // app.use(require('body-parser')());
 
 app.use(function(req, res, next) {
-	if (!res.locals.partials) res.locals.partials = {};
-	res.locals.partials.menuContext = appdata.getNavMenu();
-	next();
+  if (!res.locals.partials) res.locals.partials = {};
+  res.locals.partials.menuContext = appdata.getNavMenu();
+  next();
 });
 
 app.get('/', function(req, res) {
-	res.render('home');
+  res.render('home');
 });
 
 app.get('/users', function(req, res) {
-	res.render('users');
+
+  var users = [];
+  pool.getConnection(function(err, connection) {
+    connection.query('SELECT id, name, phone, email, alive FROM users', function(error, results, fields) {
+      connection.release();
+
+      if (error) console.error(error);
+      users = {
+        message: "success",
+        counted: results.length,
+        data: results,
+      };
+      res.render('users', {
+        users: users
+      });
+    });
+  });
 });
 
 app.get('/obituary', function(req, res) {
-	res.render('obituary');
+  res.render('obituary');
 });
 
 app.get('/sms', function(req, res) {
-	res.render('sms');
+  res.render('sms');
 });
 
 // 404 catch-all handler (middleware)
-app.use(function(req, res, next){
-	res.status(404);
-	res.render('404');
+app.use(function(req, res, next) {
+  res.status(404);
+  res.render('404');
 });
 
 // 500 error handler (middleware)
-app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.status(500);
-	res.render('500');
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log( 'Express started on http://localhost:' +
-    app.get('port') + '; press Ctrl-C to terminate.' );
+app.listen(app.get('port'), function() {
+  console.log('Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.');
 });
