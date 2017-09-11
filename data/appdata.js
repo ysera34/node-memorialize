ea257bc7314945a992a3d12a817381b366fb2357
ysera34 <ycyoon34@gmail.com>
2017-09-11 11:18:31 +0900

    express handlebar partials

---
+++ data/appdata.js
@@ -0,0 +1,26 @@
+module.exports = {
+  getNavMenu: function() {
+  	return {
+  		menus: [
+  			{
+  				name: 'menu name 1',
+  				href: '/about',
+  			},
+  			{
+  				name: 'menu name 2',
+  				href: '/about',
+  			},
+  			{
+  				name: 'menu name 3',
+  				href: '/about',
+  			},
+  			{
+  				name: 'menu name 4',
+  				href: '/about',
+  			},
+  		],
+  	};
+  },
+
+
+};
---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require('express');
+  // formidable = require('formidable');
+var appdata = require('./data/appdata.js');
+
+var app = express();
+
+// set up handlebars view engine
+var handlebars = require('express-handlebars').create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+// app.use(require('body-parser')());
+
+app.use(function(req, res, next) {
+	if (!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.menuContext = appdata.getNavMenu();
+	next();
+});
+
+app.get('/', function(req, res) {
+	res.render('home');
+});
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
